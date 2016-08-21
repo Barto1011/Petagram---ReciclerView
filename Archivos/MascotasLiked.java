6349d4f94ab9c9ad12b8bcc7b73d080843eb28c5
Barto1011 <btesis1011@gmail.com>
2016-08-21 06:52:35 -0500

    Update MascotasLiked.java

--- Archivos/MascotasLiked.java
+++ Archivos/MascotasLiked.java
@@ -1,26 +1,33 @@
-package com.barto.petagramweek3;
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:context="com.barto.petagramweek3.MascotasLiked">
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.support.v7.app.ActionBar;
+    <include android:id="@+id/miActionBar"
+        layout="@layout/actionbar"></include>
+
+    <TextView
+        android:id="@+id/TituloDetalleLike"
+        android:layout_below="@id/miActionBar"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="Mascotas Liked"
+        android:textSize="20sp"
+        android:padding="7dp"
+        android:textStyle="bold"
+        android:layout_centerHorizontal="true"
+        />
+    <android.support.v7.widget.RecyclerView
+        android:id="@+id/rvMascotasLike"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:layout_centerHorizontal="true"
+        android:padding="@dimen/activity_horizontal_margin"
+        android:layout_below="@+id/TituloDetalleLike"
+        >
+    </android.support.v7.widget.RecyclerView>
 
 
-public class MascotasLiked extends AppCompatActivity {
-
-    public Toolbar miActionBar;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mascotas_liked);
-
-        miActionBar = (Toolbar) findViewById(R.id.miActionBar);
-        miActionBar.setTitle("Title");
-        setSupportActionBar(miActionBar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setHomeAsUpIndicator(R.drawable.petagram_back);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-}
+</RelativeLayout>
