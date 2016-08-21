9bf640aa9401904881f495698e9c480d33bdbda2
Barto1011 <btesis1011@gmail.com>
2016-08-21 06:56:18 -0500

    Update MascotasLiked.java

--- Archivos/MascotasLiked.java
+++ Archivos/MascotasLiked.java
@@ -1,33 +1,58 @@
-<?xml version="1.0" encoding="utf-8"?>
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    tools:context="com.barto.petagramweek3.MascotasLiked">
+package com.barto.petagramweek3;
 
-    <include android:id="@+id/miActionBar"
-        layout="@layout/actionbar"></include>
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBar;
 
-    <TextView
-        android:id="@+id/TituloDetalleLike"
-        android:layout_below="@id/miActionBar"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="Mascotas Liked"
-        android:textSize="20sp"
-        android:padding="7dp"
-        android:textStyle="bold"
-        android:layout_centerHorizontal="true"
-        />
-    <android.support.v7.widget.RecyclerView
-        android:id="@+id/rvMascotasLike"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        android:layout_centerHorizontal="true"
-        android:padding="@dimen/activity_horizontal_margin"
-        android:layout_below="@+id/TituloDetalleLike"
-        >
-    </android.support.v7.widget.RecyclerView>
+import java.util.ArrayList;
 
 
-</RelativeLayout>
+public class MascotasLiked extends AppCompatActivity {
+
+    public Toolbar miActionBar;
+    ArrayList<Mascota> mascotas;
+    private RecyclerView listaMascotas;
+    public MascotaAdaptador adaptador;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mascotas_liked);
+
+        miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        miActionBar.setTitle("Title");
+        setSupportActionBar(miActionBar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.petagram_back);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        listaMascotas = (RecyclerView) findViewById(R.id.rvMascotasLike);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        listaMascotas.setLayoutManager(llm);
+
+        inicializarListaMascotasFav();
+        inicializarAdaptador();
+    }
+    private void inicializarAdaptador() {
+        adaptador = new MascotaAdaptador(mascotas, this);
+        listaMascotas.setAdapter(adaptador);
+
+    }
+
+    public void inicializarListaMascotasFav(){
+
+        mascotas = new ArrayList<Mascota>();
+
+        mascotas.add(new Mascota (R.drawable.petagram_dog_5, "Lucas", "1"));
+        mascotas.add(new Mascota (R.drawable.petagram_dog_4, "Pedro", "2"));
+        mascotas.add(new Mascota (R.drawable.petagram_dog_3, "Ramon", "3"));
+        mascotas.add(new Mascota (R.drawable.petagram_dog_7, "Bernardo", "4"));
+        mascotas.add(new Mascota (R.drawable.petagram_dog_1, "bonso", "5"));
+    }
+}
